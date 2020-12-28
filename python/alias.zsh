06ecca16a22d6ac49751e6e51c1b7c696e900b06
ackneal <neal.anglo@gmail.com>
2020-12-28 23:28:47 +0800

    feat: add virtualenvwrapper

--- python/alias.zsh
+++ python/alias.zsh
@@ -1,4 +1,8 @@
 if [ -f "/usr/bin/python3" ]; then
         alias py=/usr/bin/python3
         [[ -z "$PYTHON_VERSION" ]] && export PYTHON_VERSION=$(py --version | awk '{print $2}' | awk -F '.' '{print $1"."$2}')
+
+        export WORKON_HOME=$HOME/.virtualenvs
+        export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
+        source $HOME/Library/Python/$PYTHON_VERSION/bin/virtualenvwrapper.sh
 fi
